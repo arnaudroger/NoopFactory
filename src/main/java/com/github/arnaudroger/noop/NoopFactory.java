48076fc0a129cbf3a149fbd4e0f7a8c800e368bc
Arnaud Roger <arnaud.roger@gmail.com>
2017-02-07 21:57:32 +0000

    #1 #2 support primitive and check is functional interface

--- src/main/java/com/github/arnaudroger/noop/NoopFactory.java
+++ src/main/java/com/github/arnaudroger/noop/NoopFactory.java
@@ -2,27 +2,101 @@ package com.github.arnaudroger.noop;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 import java.lang.reflect.Proxy;
+import java.util.HashMap;
+import java.util.Map;
 
 public class NoopFactory {
 
-    private static final InvocationHandler NULL_INVOCATION_HANDLER = new InvocationHandler() {
+    private static final InvocationHandler NULL_INVOCATION_HANDLER = new ConstantInvocationHandler(null);
+
+    private static final Map<Class<?>, Object> DEFAULT_VALUES = new HashMap<>();
+
+    static {
+        DEFAULT_VALUES.put(boolean.class, false);
+        DEFAULT_VALUES.put(byte.class, (byte)0);
+        DEFAULT_VALUES.put(char.class, (char)0);
+        DEFAULT_VALUES.put(short.class, (short)0);
+        DEFAULT_VALUES.put(int.class, (int)0);
+        DEFAULT_VALUES.put(long.class, (long)0);
+        DEFAULT_VALUES.put(float.class, (float)0);
+        DEFAULT_VALUES.put(double.class, (double)0);
+    }
+
+    private static final ClassValue<InvocationHandler> PRIMTIVES_INVOCATION_HANDLERS = new ClassValue<InvocationHandler>() {
         @Override
-        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-            return null;
+        protected InvocationHandler computeValue(Class<?> type) {
+            if (!type.isPrimitive()) {
+                throw new IllegalArgumentException("Only support primitives");
+            }
+
+            Object value = DEFAULT_VALUES.get(type);
+
+            if (value == null) {
+                throw new IllegalStateException("No default value defined for " + type);
+            }
+            return new ConstantInvocationHandler(value);
         }
     };
 
+    @SuppressWarnings("unchecked")
     public static <T> T noop(Class<?> target) {
-        if (! target.isInterface()) {
-            throw new IllegalArgumentException("Expect class to be an interface " + target);
+        Method m = getFunctionalMethod(target);
+        if (m == null) {
+            throw new IllegalArgumentException("Expect class to be a functional interface " + target);
         }
 
-        return (T) Proxy.newProxyInstance(target.getClassLoader(), new Class[]{target}, newInvotionHandler(target));
+        return (T) Proxy.newProxyInstance(target.getClassLoader(), new Class[]{target}, newInvotionHandler(m));
     }
 
-    private static InvocationHandler newInvotionHandler(Class<?> target) {
-        return NULL_INVOCATION_HANDLER;
+    static Method getFunctionalMethod(Class<?> target) {
+        if (!target.isInterface()) {
+            return null;
+        }
+
+        Method functionalMethod = null;
+        for(Method m : target.getDeclaredMethods()) {
+            if (isElegibleMethod(m)) {
+                if (functionalMethod != null) return null;
+                functionalMethod = m;
+            }
+        }
+        return functionalMethod;
     }
 
+    private static boolean isElegibleMethod(Method m) {
+        if (m.isDefault() || m.isSynthetic() || m.isBridge() || Modifier.isStatic(m.getModifiers())) {
+            return false;
+        }
+        if (m.getName().equals("equals")) {
+            return !(boolean.class.equals(m.getReturnType()) && m.getParameterCount() == 1 && Object.class.equals(m.getParameterTypes()[0]));
+        }
+        if (m.getName().equals("hashCode")) {
+            return !(int.class.equals(m.getReturnType()) && m.getParameterCount() == 0);
+        }
+        return true;
+    }
+
+    private static InvocationHandler newInvotionHandler(Method target) {
+        Class<?> returnType = target.getReturnType();
+        if (!returnType.isPrimitive()) {
+            return NULL_INVOCATION_HANDLER;
+        }
+
+        return PRIMTIVES_INVOCATION_HANDLERS.get(returnType);
+    }
+
+    private static class ConstantInvocationHandler implements InvocationHandler {
+        private final Object value;
+
+        private ConstantInvocationHandler(Object value) {
+            this.value = value;
+        }
+
+        @Override
+        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+            return value;
+        }
+    }
 }
--- src/test/java/com/github/arnaudroger/noop/NoopFactoryTest.java
+++ src/test/java/com/github/arnaudroger/noop/NoopFactoryTest.java
@@ -1,15 +1,60 @@
 package com.github.arnaudroger.noop;
 
+import org.junit.Test;
+
 import java.util.function.Function;
+import java.util.function.IntFunction;
+import java.util.function.ToIntFunction;
 
 import static org.junit.Assert.*;
 
 public class NoopFactoryTest {
-    @org.junit.Test
+    @Test
     public void noop() throws Exception {
-
         Function<Integer, String> function = NoopFactory.noop(Function.class);
         assertNull(function.apply(3));
+    }
+
+    @Test
+    public void noopToInt() throws Exception {
+        ToIntFunction<Void> function = NoopFactory.noop(ToIntFunction.class);
+        assertEquals(0, function.applyAsInt(null));
+    }
+
+    @Test
+    public void noopToVoid() throws Exception {
+        IntFunction<Void> function = NoopFactory.noop(IntFunction.class);
+        assertNull(function.apply(1));
+    }
+
+
+
+    @Test
+    public void testIsFunctionalInterface() throws NoSuchMethodException {
+        assertEquals(Function.class.getMethod("apply", Object.class), NoopFactory.getFunctionalMethod(Function.class));
+        assertEquals(CustomFunction.class.getMethod("doWhat", Void.class), NoopFactory.getFunctionalMethod(CustomFunction.class));
+        assertEquals(CustomFunctionWithEquals.class.getMethod("doWhat", Void.class), NoopFactory.getFunctionalMethod(CustomFunctionWithEquals.class));
+        assertNull(NoopFactory.getFunctionalMethod(CustomClass.class));
+        assertNull(NoopFactory.getFunctionalMethod(Custom2Methods.class));
+    }
+
+    private interface CustomFunction {
+        void doWhat(Void v);
+    }
+
+    private interface CustomFunctionWithEquals {
+        void doWhat(Void v);
+        boolean equals(Object o);
+    }
+
+    private interface Custom2Methods {
+        void doWhat(Void v);
+        boolean test(Object o);
+    }
+
+
+    private abstract class CustomClass {
+        abstract void doWhat(Void v);
     }
 
 }
